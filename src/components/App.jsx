4c3be15fb675c25d4c5ff312405ecfca8751eef4
Serhii Moroz <sergiy.m.lutsk@gmail.com>
2023-01-24 21:03:01 +0200

    Appbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import { Menu } from "pages/Menu";
+import { Navigate } from "react-router-dom";
+
+
 export const App = () => {
+  
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/menu" element={<Menu/>} />
+        {/* <Route path="/menu/:menuGroup" element={<MenuGroup/>} />       */}
+        {/* <Route path="/about" element={<About/>} /> */}
+        <Route path="*" element={<Navigate to={'/menu'} />} />
+      </Routes>
+    </>
   );
 };
---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,41 @@
+import { ReactComponent as MenuIcon } from '../../svg/menu.svg';
+import { ReactComponent as CloseMenuIcon } from '../../svg/close-menu-button.svg';
+import { ReactComponent as ClosePhoneIcon } from '../../svg/close-phone-button.svg';
+import { ReactComponent as Phone } from '../../svg/phone.svg';
+import { ReactComponent as YellowPhone } from '../../svg/yellow-phone.svg';
+import { ReactComponent as Instagram } from '../../svg/instagram.svg';
+import { ReactComponent as Facebook } from '../../svg/facebook.svg';
+import logo from '../../images/logo-mobile.png';
+import css from './AppBar.module.css'
+import { useState } from 'react';
+
+export const AppBar = () => {
+  const [isModal, setIsModal] = useState(false);
+  const [isPhone, setIsPhone] = useState(false);
+  return (
+    <>
+      <div className={css.AppBar}>
+        {!isModal && <button style={{ marginRight: '25px'}} onClick={() => setIsModal(true)}><MenuIcon/></button >}
+        {isModal && <button style={{ marginRight: '25px'}} onClick={() => setIsModal(false)}><CloseMenuIcon/></button >}
+        <img src={logo} alt='logo' />
+        <div style={{ display: 'flex', marginLeft: 'auto', alignItems: 'center' }}>
+          {isPhone && (
+            <>
+              <YellowPhone style={{ marginRight: '10px' }} />
+              <p className={css.phoneNumber}>098-60-777-99</p>
+              <button onClick={() => setIsPhone(false)} ><ClosePhoneIcon/></button >
+            </>
+          )}
+          {!isPhone && (
+            <>
+              <button style={{ marginRight: '20px' }} onClick={() => setIsPhone(true)} ><Phone /></button >
+              <Instagram style={{marginRight: '20px' }} />
+              <Facebook />
+            </>
+          )}
+        </div>
+      </div>
+      
+    </>
+  );
+};
