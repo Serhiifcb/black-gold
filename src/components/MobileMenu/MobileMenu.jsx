705868dd8e42371e28db7afdf255a926cb0c2bab
Serhii Moroz <sergiy.m.lutsk@gmail.com>
2023-02-02 11:10:19 +0200

    fixed Mobile Menu

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -11,7 +11,7 @@ import { animateScroll as scroll} from 'react-scroll'
 export const MobileMenu = () => {
   const dispatch = useDispatch();
   const activeItem = useSelector(state => state.activePage);
-  const menuItems = ["Піца", "Напої", "Перші страви", "Другі страви", "Десерти", "Закуски", "Гаряче", "Комплексні обіди"];
+  // const menuItems = ["Піца", "Напої", "Перші страви", "Другі страви", "Десерти", "Закуски", "Гаряче", "Комплексні обіди"];
   const menu = useSelector(state => state.kitchenMenu.menu);
   const changeActivePage = event => {
     dispatch(setActivePage(event.target.textContent));
